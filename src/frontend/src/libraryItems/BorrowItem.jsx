86f1d304736e5628352ba825d192361db0416120
rannerhall <r.annerhall@hotmail.com>
2021-07-20 14:16:23 +0200

    .

--- src/frontend/src/libraryItems/BorrowItem.jsx
+++ src/frontend/src/libraryItems/BorrowItem.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import {alertService, libraryItemService} from "@/_services";
+import {useForm} from "react-hook-form";
+import {Link} from "react-router-dom";
 
-const Borrower = () => (
-    <div className="form-group col-5">
-        <label>Borrower</label>
-        <input name="borrower" type="text" ref={register}
-               className={`form-control ${errors.borrower ? 'is-invalid' : ''}`}/>
-        <div className="invalid-feedback">{errors.borrower?.message}</div>
-    </div>
-)
+function Borrower({history, match}) {
+    const {id} = match.params;
+
+    const {register, handleSubmit, reset, setValue, errors, formState} = useForm({});
+
+    function onSubmit(data) {
+        return checkOutLibraryItem(id, data);
+    }
+
+    function checkOutLibraryItem(id, data) {
+        return libraryItemService.update(id, data)
+            .then(() => {
+                alertService.success('Checkout complete', {keepAfterRouteChange: true});
+                history.push('..');
+            })
+            .catch(alertService.error);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} onReset={reset}>
+            <div className="form-group col-5">
+                <label>Borrower</label>
+                <input name="borrower" type="text" ref={register}
+                       className={`form-control ${errors.borrower ? 'is-invalid' : ''}`}/>
+                <div className="invalid-feedback">{errors.borrower?.message}</div>
+            </div>
+            <div className="form-group">
+                <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary">
+                    {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"/>}
+                    Save
+                </button>
+                <Link className="btn btn-link">Cancel</Link>
+            </div>
+        </form>
+    )
+}
+
+export default Borrower
--- src/frontend/src/libraryItems/LibraryItemsIndex.jsx
+++ src/frontend/src/libraryItems/LibraryItemsIndex.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import { LibraryItemsList } from './LibraryItemsList';
 import { LibraryItemsAddEdit } from './LibraryItemsAddEdit';
+import BorrowItem from "@/libraryItems/BorrowItem";
 
 function LibraryItems({ match }) {
     const { path } = match;
@@ -12,6 +13,7 @@ function LibraryItems({ match }) {
             <Route exact path={path} component={LibraryItemsList} />
             <Route path={`${path}/add`} component={LibraryItemsAddEdit} />
             <Route path={`${path}/edit/:id`} component={LibraryItemsAddEdit} />
+            <Route path={`${path}/borrow/:id`} component={BorrowItem} />
         </Switch>
     );
 }
